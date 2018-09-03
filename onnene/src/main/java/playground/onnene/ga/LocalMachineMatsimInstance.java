6ca4ab944711d208e62b693abf4dc4c3d52c19d0
Onnene <obinnene@gmail.com>
2018-09-03 14:13:26 +0200

    Added SBB Raptor router

--- onnene/src/main/java/playground/onnene/ga/LocalMachineMatsimInstance.java
+++ onnene/src/main/java/playground/onnene/ga/LocalMachineMatsimInstance.java
@@ -24,9 +24,13 @@ package playground.onnene.ga;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy.OverwriteFileSetting;
 import org.matsim.core.scenario.ScenarioUtils;
+
+import ch.sbb.matsim.mobsim.qsim.SBBQSimModule;
+import ch.sbb.matsim.routing.pt.raptor.SwissRailRaptorModule;
 
 /**
  * @author Onnene
@@ -59,6 +63,19 @@ public class LocalMachineMatsimInstance {
 
 		Scenario scenario = ScenarioUtils.loadScenario(config);
 		Controler controler = new Controler(scenario);
+		
+		controler.addOverridingModule(new AbstractModule() {
+			@Override
+			public void install() {
+				// To use the deterministic pt simulation:
+				install(new SBBQSimModule());
+
+				// To use the fast pt router:
+				install(new SwissRailRaptorModule());
+			}
+		});
+		
+		
 
 		controler.run();
 	}
