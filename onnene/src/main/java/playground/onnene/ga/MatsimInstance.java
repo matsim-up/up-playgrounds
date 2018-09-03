38e20b991059b9a566a9903b17cc8b9b5b05230b
Onnene <obinnene@gmail.com>
2018-09-03 16:46:56 +0200

    minor fixes

--- onnene/src/main/java/playground/onnene/ga/MatsimInstance.java
+++ onnene/src/main/java/playground/onnene/ga/MatsimInstance.java
@@ -24,9 +24,13 @@ package playground.onnene.ga;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy.OverwriteFileSetting;
 import org.matsim.core.scenario.ScenarioUtils;
+
+import ch.sbb.matsim.mobsim.qsim.SBBQSimModule;
+import ch.sbb.matsim.routing.pt.raptor.SwissRailRaptorModule;
 
 /**
  * Class to execute a single MATSim run in its own virtual machine.
@@ -69,6 +73,17 @@ public class MatsimInstance{
 
 		Scenario scenario = ScenarioUtils.loadScenario(config);
 		Controler controler = new Controler(scenario);
+		
+		controler.addOverridingModule(new AbstractModule() {
+			@Override
+			public void install() {
+				// To use the deterministic pt simulation:
+				install(new SBBQSimModule());
+
+				// To use the fast pt router:
+				install(new SwissRailRaptorModule());
+			}
+		});
 
 		controler.run();
 	}
