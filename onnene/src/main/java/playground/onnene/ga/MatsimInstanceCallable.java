4b858bee83ab7dd6533b1196efe33a1fcad40451
Onnene <obinnene@gmail.com>
2018-10-13 23:22:28 +0200

    minor fix

--- onnene/src/main/java/playground/onnene/ga/MatsimInstanceCallable.java
+++ onnene/src/main/java/playground/onnene/ga/MatsimInstanceCallable.java
@@ -148,7 +148,7 @@ public class MatsimInstanceCallable implements Callable<Double[]> {
 				"java",
 				"-Xmx5g",
 				"-cp",
-				String.format(".%s./onnene-0.10.0-SNAPSHOT/onnene-0.10.0-SNAPSHOT.jar", File.pathSeparator),
+				String.format(".%s/onnene-0.10.0-SNAPSHOT/onnene-0.10.0-SNAPSHOT.jar", File.pathSeparator),
 				"playground.onnene.ga.MatsimInstance",
 				"config.xml",
 				//String.format("%s", folder.getAbsolutePath()+File.separator+"output"),
