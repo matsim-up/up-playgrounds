b0ff98ffdc8eaebbf487fa210d68a8e856215469
Onnene <obinnene@gmail.com>
2019-07-16 13:45:59 +0200

    Final param setting for experiment 6 - multiple algorithms - NSGA2, NSGA3, SPEA2

--- onnene/src/main/java/playground/onnene/ga/RunSimulationBasedTransitOptimisation.java
+++ onnene/src/main/java/playground/onnene/ga/RunSimulationBasedTransitOptimisation.java
@@ -206,9 +206,9 @@ public class RunSimulationBasedTransitOptimisation {
 		//properties.setDoubleArray("weights", new double[] {0.2, 0.8});
 		properties.setInt("populationSize", POP_SIZE);
 		
-		String[] algorithmNames = new String[] {"NSGA-II"};
+		//String[] algorithmNames = new String[] {"NSGA-II"};
 
-		//String[] algorithmNames = new String[] { "NSGA-II", "NSGA-III", "DBEA", "SPEA2", "IBEA" };
+		String[] algorithmNames = new String[] {"NSGA-II","NSGA-III","SPEA2"};
 		//String[] algorithmNames = new String[] {"GA"}
 		
 		List<File> outputFiles = new ArrayList<File>();
