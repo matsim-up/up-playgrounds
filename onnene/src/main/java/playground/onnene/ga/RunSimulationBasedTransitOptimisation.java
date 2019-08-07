47eb1dcb1723d41e0e9a823fb2b4f4022ed85443
Onnene <obinnene@gmail.com>
2019-08-07 10:30:39 +0200

    Setting matsim ensemble run to 50 instances
    Check how long this setting will run for

--- onnene/src/main/java/playground/onnene/ga/RunSimulationBasedTransitOptimisation.java
+++ onnene/src/main/java/playground/onnene/ga/RunSimulationBasedTransitOptimisation.java
@@ -60,7 +60,7 @@ public class RunSimulationBasedTransitOptimisation {
 	private static final Logger log = Logger.getLogger(RunSimulationBasedTransitOptimisation.class);
 	
     private static final int MAX_NFE = 1000;   
-	private static final int POP_SIZE = 10;
+	private static final int POP_SIZE = 100;
 	private static final int CHECKPOINT_FREQ = POP_SIZE;
 	public static final int MATSIM_ITERATION_NUMBER = 80;
     private static FileOutputStream SEED_FILE, REFSET_TXT, REFSET_PF, MOEA_LOG;
