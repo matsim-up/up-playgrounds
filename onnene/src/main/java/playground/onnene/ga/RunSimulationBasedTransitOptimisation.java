81b2442ec452a665bcae7a458e505ccb965bce58
Onnene <obinnene@gmail.com>
2019-11-15 23:44:38 +0200

    Update RunSimulationBasedTransitOptimisation.java

--- onnene/src/main/java/playground/onnene/ga/RunSimulationBasedTransitOptimisation.java
+++ onnene/src/main/java/playground/onnene/ga/RunSimulationBasedTransitOptimisation.java
@@ -57,8 +57,8 @@ public class RunSimulationBasedTransitOptimisation {
 	
 	private static final Logger log = Logger.getLogger(RunSimulationBasedTransitOptimisation.class);
 	
-    private static final int MAX_NFE = 100;   
-	private static final int POP_SIZE = 10;
+    private static final int MAX_NFE = 300;   
+	private static final int POP_SIZE = 30;
 	private static final int CHECKPOINT_FREQ = POP_SIZE;
 	public static final int MATSIM_ITERATION_NUMBER = 10;
     private static FileOutputStream SEED_FILE, REFSET_TXT, REFSET_PF, MOEA_LOG;
